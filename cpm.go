e69e1705a6759e8ef1434cd68f4568ff69494ac4
Mircea Neacsu <mircea@neacsu.net>
2021-11-23 00:10:53 -0500

    Fixed circular dependency problem

--- cpm.go
+++ cpm.go
@@ -34,25 +34,32 @@ import (
 	"github.com/go-git/go-git/v5"
 )
 
-type BuildSet struct {
+type BuildCommands struct {
 	Os   string
 	Cmd  string
 	Args []string
 }
 
+type DependencyDescriptor struct {
+	Name      string
+	Git       string
+	FetchOnly bool
+	pack      *PacUnit
+}
+
 type PacUnit struct {
 	Name    string
 	Git     string
-	Build   []*BuildSet
-	Depends []*PacUnit
+	Build   []BuildCommands
+	Depends []DependencyDescriptor
+	built   bool
 }
 
 var devroot string         //root of development tree
 var root_descriptor string //path of root module
 const descriptor_name = "cpm.json"
 
-var fetched map[string]bool
-var built map[string]bool
+var all_packs []*PacUnit
 
 var inprocess []string
 
@@ -94,14 +101,15 @@ func main() {
 	Verboseln("DEV_ROOT = " + devroot)
 	os.Mkdir(devroot+"lib", 0755)
 
-	fetched = make(map[string]bool)
-	var root PacUnit
+	root := new(PacUnit)
+	all_packs = append(all_packs, root)
+
 	var data []byte
 	if data, err = os.ReadFile(root_descriptor); err != nil {
 		log.Fatalf("cannot open '%s' file", root_descriptor)
 	}
 
-	if err = json.Unmarshal(data, &root); err != nil {
+	if err = json.Unmarshal(data, root); err != nil {
 		log.Fatalf("cannot parse %s - %v\n", root_descriptor, err)
 	}
 
@@ -110,20 +118,16 @@ func main() {
 	cwd, _ := os.Getwd()
 	Verboseln("Changed directory to " + cwd)
 
-	inprocess = make([]string, 0, 10)
-	fetch(&root)
+	fetch(root)
+
 	if !*fetch_flag {
-		built = make(map[string]bool)
-		build(&root)
+		inprocess = make([]string, 0, 10)
+		build(root)
 	}
 }
 
 // Fetch a package and all its dependents
 func fetch(p *PacUnit) {
-	if fetched[p.Name] {
-		Verboseln("Package " + p.Name + " has already been configured")
-		return
-	}
 	pacdir := devroot + p.Name
 	if os.Chdir(pacdir) != nil {
 		if err := os.Mkdir(pacdir, 0666); err != nil {
@@ -153,13 +157,33 @@ func fetch(p *PacUnit) {
 			log.Fatalf("cannot parse %s - %v", descriptor_name, err)
 		}
 	}
-	fetched[p.Name] = true
 
 	if p.Depends != nil {
 		//setup all dependent packages
-		var d *PacUnit
-		for _, d = range p.Depends {
-			fetch(d)
+		for i := range p.Depends {
+			var v *PacUnit
+			var idx int
+
+			//search if already setup
+			found := false
+			for idx, v = range all_packs {
+				if v.Name == p.Depends[i].Name {
+					found = true
+					break
+				}
+			}
+			if !found {
+				//add new package
+				d := new(PacUnit)
+				all_packs = append(all_packs, d)
+				d.Name = p.Depends[i].Name
+				d.Git = p.Depends[i].Git
+				fetch(d)
+				p.Depends[i].pack = d
+			} else {
+				p.Depends[i].pack = all_packs[idx]
+				Verbosef("Package %s has already been configured\n", p.Depends[i].Name)
+			}
 		}
 
 		if os.Chdir(pacdir+"/include") != nil {
@@ -169,7 +193,7 @@ func fetch(p *PacUnit) {
 
 		//create symlinks to dependents
 		for _, dep := range p.Depends {
-			Verboseln("Creating symlink " + devroot + dep.Name + "/include/" + dep.Name + " --> " + dep.Name)
+			Verbosef("In %s creating symlink %sinclude/%[3]s --> %[3]s\n", cwd, devroot, dep.Name)
 			os.Symlink(devroot+dep.Name+"/include/"+dep.Name, dep.Name)
 		}
 	}
@@ -177,14 +201,13 @@ func fetch(p *PacUnit) {
 
 //Build a packge after having built first its dependents
 func build(p *PacUnit) {
-	if built[p.Name] {
+	if p.built {
 		Verboseln("Package " + p.Name + " has already been built")
-		fmt.Printf("Build map: % v", built)
 		return
 	}
 	for _, w := range inprocess {
 		if w == p.Name {
-			log.Fatalf("Package %s depends on itself.\n Dependency chain: %v", p.Name, inprocess)
+			log.Fatalf("Package %s depends on itself.\n Dependency chain: %v\n", p.Name, inprocess)
 		}
 	}
 
@@ -197,19 +220,23 @@ func build(p *PacUnit) {
 
 	//First, build all dependent packages
 	if p.Depends != nil {
-		var d *PacUnit
+		var d DependencyDescriptor
 		for _, d = range p.Depends {
-			build(d)
-			os.Chdir(cwd)
+			if !d.FetchOnly {
+				build(d.pack)
+			} else {
+				Verbosef("Package %s - skipped build\n", d.Name)
+			}
 		}
+		os.Chdir(cwd)
 	}
 
 	// then build self
 	if ret, err := do_build(p.Build); ret != 0 {
-		log.Fatalf("Build aborted - %v", err)
+		log.Fatalf("Build aborted - %v\n", err)
 	}
 	inprocess = inprocess[:len(inprocess)-1]
-	built[p.Name] = true
+	p.built = true
 }
 
 /*
@@ -217,7 +244,7 @@ func build(p *PacUnit) {
 	command for the current OS envirnoment, use that one. Otherwise choose a
 	generic one (os set to "any" or "")
 */
-func do_build(b []*BuildSet) (int, error) {
+func do_build(b []BuildCommands) (int, error) {
 	for _, cfg := range b {
 		if cfg.Os == runtime.GOOS {
 			Verbosef("OS: %s cmd: %s % v\n", cfg.Os, cfg.Cmd, cfg.Args)
@@ -279,7 +306,7 @@ func Run(prog string, args []string) (int, error) {
 func clone(url, dir string) bool {
 	fullpath := devroot + dir
 
-	Verbosef("Cloning: %s in %s", url, dir)
+	Verbosef("Cloning: %s in %s\n", url, dir)
 
 	if stat, err := Run("git", []string{"clone", url, fullpath}); err != nil || stat != 0 {
 		log.Fatalf("Cloning failed \nStatus %d Error: %v\n", stat, err)
